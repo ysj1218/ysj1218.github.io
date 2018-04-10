f062c56090df62bec0fcb64732ac94c13f86e6bb
ysj1218 <2519512987@qq.com>
2018-04-10 17:58:32 +0800

    grunt

---
+++ grunt配置文件/demo/src/js/2.js
@@ -0,0 +1,6 @@
+function url(url) {
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	script.url = url;
+	document.body.appendChild(script);
+}
---
+++ grunt配置文件/demo/src/js/concat/concat.js
@@ -0,0 +1,9 @@
+/*!grunt - 1.0.0-2018-04-10 */function add(x, y) {
+	return x+y;
+}
+function url(url) {
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	script.url = url;
+	document.body.appendChild(script);
+}
