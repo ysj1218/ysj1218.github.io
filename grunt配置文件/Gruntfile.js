0980dc46f6d33e2187305148a7627fc27a12b6a2
ysj1218 <2519512987@qq.com>
2018-04-12 18:17:38 +0800

    gruntfileChange

--- grunt配置文件/Gruntfile.js
+++ grunt配置文件/Gruntfile.js
@@ -56,16 +56,30 @@ module.exports = function(grunt) {
 			build: ['src/css/*.css']
 
 		},
+
+		//grunt将es6转换为es5
+		babel: {
+			options: {
+				sourceMap: true,
+				presets: ['env']
+			},
+			dist: {
+				files: {
+					'dist/app.js': 'src/app.js'
+				}
+			}
+		},
+
 		//watch自动化
 		watch: {
 			build: {
 				files: ['src/js/*.js', 'src/css/*.css'],
-				tasks: ['jshint', 'csslint', 'concat', 'cssmin', 'uglify'],
+				tasks: ['jshint', 'csslint', 'concat', 'cssmin', 'uglify', 'babel'],
 				options: {
 					spawn: false
 				}
 			}
-		}
+		},
 
 	});
 	//告诉grunt我们将使用插件
@@ -74,7 +88,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-babel');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+
 	//告诉grunt当我们在终端输入grunt时需要做些什么
-	grunt.registerInitTask('default', ['jshint', 'csslint', 'concat', 'cssmin', 'uglify', 'watch']); //先进行语法检查，如果没有问题，再合并，再压缩
+	grunt.registerInitTask('default', ['jshint', 'csslint', 'concat', 'cssmin', 'uglify', 'babel', 'watch']); //先进行语法检查，如果没有问题，再合并，再压缩
 };
